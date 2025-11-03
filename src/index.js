351150fe8f596e0773af7d0f90c4323c257c587c
tatsumiakahori <seadragon0828@gmail.com>
2025-11-03 09:10:47 +0900

    データソースをつなぎこむ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+// DBセットアップのために必要
+const store = createStore();
+
+const server = new ApolloServer({
+    typeDefs,
+    dataSources: () => {
+        return {
+            launchAPI: new LaunchAPI(),
+            userAPI: new UserAPI({ store }),
+        };
+    }
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
