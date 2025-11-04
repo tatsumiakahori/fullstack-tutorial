3a1438c4ac90d210463ffb0d22a7e6d7e6b5600f
tatsumiakahori <seadragon0828@gmail.com>
2025-11-05 07:04:10 +0900

    a

--- src/index.js
+++ src/index.js
@@ -8,15 +8,26 @@ const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
 const store = createStore();
+const isEmail = require('isemail');
 
 const server = new ApolloServer({
-  typeDefs,
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+        if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] || null;
 
-  resolvers,
-  dataSources: () => ({
-    launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI({ store })
-  })
+        return { user: { ...user.dataValues } };
+    },
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
 });
 
 server.listen().then(({ url }) => {
